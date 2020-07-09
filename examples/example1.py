4175e78631bb3ec4b417949bd37bb8bfb31422b1
Gary Smart <gary@smart-itc.com.au>
2020-07-10 09:49:47 +1000

    Updates

--- examples/example1.py
+++ examples/example1.py
@@ -1,9 +1,9 @@
 # NOTE: The import has a _ not - in the module name.
-from pigpio_dht import dht11, dht22
+from pigpio_dht import DHT11, DHT22
 
 gpio = 21 # BCM Numbering
 
-sensor = dht11(gpio)
+sensor = DHT11(gpio)
 #sensor = DHT22(gpio)
 
 result = sensor.read()
